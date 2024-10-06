d3474245508e1851e771b4dcfe2b515624b893e1
ChikiBambonni <7exalted7@gmail.com>
2024-10-06 12:38:03 +0300

    feat | add primeng grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { Table, TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterOutlet,
+    TableModule,
+    InputTextModule,
+    DropdownModule,
+    MultiSelectModule,
+    ProgressBarModule,
+    ButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'ng-app-cloud-computing';
+  searchValue: string | undefined;
+
+  musicItems = [
+    {
+      album: 'Different Class',
+      band: 'Pulp',
+      id: '2',
+      releaseDate: '1995-10-30',
+    },
+    {
+      album: 'Nevermind',
+      band: 'Nirvana',
+      id: '1',
+      releaseDate: '1991-09-24',
+    },
+  ];
+
+  clear(table: Table): void {
+    table.clear();
+    this.searchValue = '';
+  }
 }
